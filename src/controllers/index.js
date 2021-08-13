58dac16e5c2b5e5bb87587ba778d4a3e191daf3f
oss.yanuardi <yanuardi48@gmail.com>
2021-08-13 22:04:50 +0700

    add circular, tes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,13 @@
 const Api = require('../api');
+const circular = require('circular-json');
 
 exports.getTransactionStatus = async (transactionID) => {
   try {
     const response = await Api.get(`${transactionID}/status`);
-    return response;
+    let res = circular.stringify(response);
+    return res;
   } catch (error) {
-    return error;
+    let err = circular.stringify(error);
+    return err;
   }
 }
